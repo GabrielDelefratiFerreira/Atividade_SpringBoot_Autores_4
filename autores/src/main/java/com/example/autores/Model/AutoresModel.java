7fc6a7cae12a3b454a23f27ba923ab86bb99454c
Gabriel Delefrati <168195936+GabrielDelefratiFerreira@users.noreply.github.com>
2026-03-22 17:07:19 -0400

    Model criada

---
+++ autores/src/main/java/com/example/autores/Model/AutoresModel.java
@@ -0,0 +1,53 @@
+package com.example.autores.Model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_autor")
+public class AutoresModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    @Column(name = "País de Origem")
+    private String nacionalidade;
+
+    @Column(name = "Data de nascimento")
+    private LocalDate dataNascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
